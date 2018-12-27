82909895f3f0728c1835359746872a3a1c68f8c3
iskybow <kjionmd@gmail.com>
2018-12-27 17:20:01 +0300

    edits

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -61,6 +61,28 @@ $(document).ready(function () {
   }
 });
 
+window.onload = function () {
+  if (window.innerWidth > 993) {
+    $('.jsNewsFlag:lt(' + 3 + ')').fadeIn().css({ 'display': 'flex' }).removeClass('jsNewsFlag');
+
+    $('.jsShowNews').click(function () {
+      $('.jsNewsFlag:lt(' + 3 + ')').fadeIn().css({ 'display': 'flex' }).removeClass('jsNewsFlag');
+    });
+  } else if (window.innerWidth < 992 && window.innerWidth > 769) {
+    $('.jsNewsFlag:lt(' + 2 + ')').fadeIn().css({ 'display': 'flex' }).removeClass('jsNewsFlag');
+
+    $('.jsShowNews').click(function () {
+      $('.jsNewsFlag:lt(' + 2 + ')').fadeIn().css({ 'display': 'flex' }).removeClass('jsNewsFlag');
+    });
+  } else if (window.innerWidth < 768) {
+    $('.jsNewsFlag:lt(' + 1 + ')').fadeIn().css({ 'display': 'flex' }).removeClass('jsNewsFlag');
+
+    $('.jsShowNews').click(function () {
+      $('.jsNewsFlag:lt(' + 1 + ')').fadeIn().css({ 'display': 'flex' }).removeClass('jsNewsFlag');
+    });
+  }
+};
+
 $(document).ready(function () {
   $('.jsPartnersSlider').slick({
     dots: false,
@@ -98,28 +120,6 @@ $(document).ready(function () {
     }]
   });
 });
-
-window.onload = function () {
-  if (window.innerWidth > 993) {
-    $('.jsNewsFlag:lt(' + 3 + ')').fadeIn().css({ 'display': 'flex' }).removeClass('jsNewsFlag');
-
-    $('.jsShowNews').click(function () {
-      $('.jsNewsFlag:lt(' + 3 + ')').fadeIn().css({ 'display': 'flex' }).removeClass('jsNewsFlag');
-    });
-  } else if (window.innerWidth < 992 && window.innerWidth > 769) {
-    $('.jsNewsFlag:lt(' + 2 + ')').fadeIn().css({ 'display': 'flex' }).removeClass('jsNewsFlag');
-
-    $('.jsShowNews').click(function () {
-      $('.jsNewsFlag:lt(' + 2 + ')').fadeIn().css({ 'display': 'flex' }).removeClass('jsNewsFlag');
-    });
-  } else if (window.innerWidth < 768) {
-    $('.jsNewsFlag:lt(' + 1 + ')').fadeIn().css({ 'display': 'flex' }).removeClass('jsNewsFlag');
-
-    $('.jsShowNews').click(function () {
-      $('.jsNewsFlag:lt(' + 1 + ')').fadeIn().css({ 'display': 'flex' }).removeClass('jsNewsFlag');
-    });
-  }
-};
 
 $(document).ready(function () {
   $('.jsServiceSlider').slick({
